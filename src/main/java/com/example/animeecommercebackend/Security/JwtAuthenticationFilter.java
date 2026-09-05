fe2f1a30c37200934bbc6966008b92243fa17abf
Nhazz <manjirokys@gmail.com>
2026-09-05 10:42:11 +0700

    Today works: rebuild SecurityConfig, build CustomerUserDetailService, JwtAuthenticationFilter

---
+++ src/main/java/com/example/animeecommercebackend/Security/JwtAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package com.example.animeecommercebackend.Security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component // component tell spring to manage an object of this class
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomerUserDetailService customerUserDetailService;
+
+    public JwtAuthenticationFilter(JwtService jwtService,
+                                   CustomerUserDetailService customerUserDetailService) {
+        this.jwtService = jwtService;
+        this.customerUserDetailService = customerUserDetailService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Get Authorization header
+        String authHeader = request.getHeader("Authorization");
+
+        // No token
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // remove bearer
+        String jwt = authHeader.substring(7);
+
+        // get email from token
+        String email = jwtService.extractUsername(jwt);
+
+        // check if users is not already authenticated
+        if(email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = customerUserDetailService.loadUserByUsername(email);
+
+            // validate token
+            if(jwtService.isTokenValid(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(userDetails,
+                        null,
+                        userDetails.getAuthorities());
+
+                authentication.setDetails(
+                        new WebAuthenticationDetailsSource()
+                                .buildDetails(request)
+                );
+
+                // Tell spring security who is the user
+                SecurityContextHolder
+                        .getContext()
+                        .setAuthentication(authentication);
+            }
+        }
+        // continue request
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/example/animeecommercebackend/Security/SecurityConfig.java
+++ src/main/java/com/example/animeecommercebackend/Security/SecurityConfig.java
@@ -1,40 +1,105 @@
 package com.example.animeecommercebackend.Security;
 
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+import java.util.List;
+
+@Configuration // Tell that This class contains configuration that Spring needs to load.
+@RequiredArgsConstructor
 public class SecurityConfig {
+   private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+   // password encoder
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    // security filter chains
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
+                // disable csrf
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/api/auth/register",
+
+                // enable CORS
+                .cors(cors-> cors.configurationSource(corsConfiguration()))
+
+                // jwt is stateless
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // authorization rules
+                .authorizeHttpRequests(auth->auth
+
+                        // Public endpoint
+                        .requestMatchers(
+                                "/api/auth/register",
                                 "/api/auth/login",
+
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**")
-                        .permitAll()
-                        .anyRequest().permitAll()
+                                "/v3/api-docs/**"
+                        ).permitAll()
+
+                        // every other end points requires JWT
+                        .anyRequest().authenticated()
+                )
+
+                // add jwt filter
+                .addFilterBefore(jwtAuthenticationFilter,
+                                  UsernamePasswordAuthenticationFilter.class
                 );
         return httpSecurity.build();
     }
 
-    private CorsConfigurationSource corsConfigurationSource() {
+    // cors Configuration
+    private CorsConfigurationSource corsConfiguration() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        return null;
+        configuration.setAllowedOrigins(
+                List.of(
+                        "http://localhost:3000",
+                        "http://localhost:5173"
+                )
+        );
+
+        // http method
+        configuration.setAllowedMethods(
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "PATCH",
+                        "OPTIONS"
+                )
+        );
+        // header
+        configuration.setAllowedHeaders(
+                List.of("*")
+        );
+        // credentials
+        configuration.setAllowCredentials(true);
+
+        // apply cors configuration to all endpoints
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+                );
+        return source;
     }
 
 }
